ae6b672252ac89617131b33335f3440d1a30d751
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-08-05 19:51:35 -0700

    Added rolling/moving average graph

--- FlukePlotly.py
+++ FlukePlotly.py
@@ -7,41 +7,41 @@
 #   Todo: double check any code from AI and for matplotlib that's redundant (ex. update_scroll func, is fig.canvas.draw_idle() needed?) useful for optimizing program's memory and speed
 #   Todo: Add error handling
 #   Todo: Don't hard code, use constants instead
-#   Todo: graph scrolling too laggy right now\
 #   Todo: when you use zoom in feature, after a few sec it should also go back to following scroll bar at regular size
-#   Todo: if timeout or serial connection, try have python script reset the port again
+#   Todo: if timeout or serial connection, try have python script reset the port again auto
 #   Todo: When mouse hovers above the plotted line should return a data point
-#   Moving average
-#   Remove redundant timecnt value
-#   Make timestamp and data of csv and chart be consistent
+
+"""
+https://stackoverflow.com/questions/50390506/how-to-make-serial-communication-from-python-dash-server
+"""
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
-import numpy as np
-import pandas as pd
-import threading
 import datetime
 import csv
 import instruments as ik
 import re
 from itertools import count
-import random
+import numpy as np
 
 CsvWrite = False    # Csv file writing on/off
 
 INTERVAL = 100       # Amount of seconds you want in interval window (multiplied by 2)
 DELAY = 1            # Number of seconds between measurements
+ROLLING_AVG_MEASURE = 10
 BAUD = 115200
 PORT = "\\\\.\\COM3"
 FILENAME = "voltage_data-" + datetime.datetime.now().strftime('%Y-%m-%d') + "_" + datetime.datetime.now().strftime('%H_%M_%S') +".csv"
+
+
 xval = []
 yval = []
+yval_rolling = []
 timeval = []
 for i in range(INTERVAL):
     timeval.append(" ")
 timesec = count()
-timecnt = 0
 
 mult = ik.fluke.Fluke3000.open_serial(PORT, BAUD)
 
@@ -54,21 +54,13 @@ def CsvWriteData(name, data, time):
         dateStamp = now.strftime('%Y-%m-%d') + " " + time
         csvwriter.writerow([dateStamp, data])
 
-
-
-
-
-
-
-
-
-
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Layout of the Dash app
 app.layout = html.Div([
-    dcc.Graph(id='live-update-graph'),
+    dcc.Graph(id='live-update-graph-1'),  # First graph
+    dcc.Graph(id='live-update-graph-2'),  # Second graph
     dcc.Interval(
         id='interval-component',
         interval=DELAY*1000,  # Update every 1 second
@@ -78,14 +70,13 @@ app.layout = html.Div([
 
 # Callback to update the graph
 @app.callback(
-    Output('live-update-graph', 'figure'),
-    Input('interval-component', 'n_intervals')
+    [Output('live-update-graph-1', 'figure'),
+     Output('live-update-graph-2', 'figure')],
+    [Input('interval-component', 'n_intervals')]
 )
 def update_graph(n):
-    global timecnt
     volt = ''
-    timecnt = next(timesec)
-    xval.append(timecnt)
+    xval.append(next(timesec))
     time = datetime.datetime.now().time().strftime("%H:%M:%S.%f")[:-4]
     timeval.insert(len(timeval) - INTERVAL, datetime.datetime.now().time().strftime("%H:%M:%S.%f")[:-4])
 
@@ -101,23 +92,39 @@ def update_graph(n):
         volt = 0
         yval.append(volt)
 
+    if len(yval) >= ROLLING_AVG_MEASURE - 1:
+        tempavg = yval[-10:]
+        yval_rolling.append(np.mean(tempavg))
+
+    
     # Write to csv file if allowed
     if CsvWrite:
         CsvWriteData(FILENAME, str(volt), time)
 
     # Create the Plotly figure
-    figure = {
+    figure1 = {
         'data': [go.Scatter(x=xval, y=yval, mode='lines+markers')],
         'layout': go.Layout(
-            title='Real-Time Updating Line Graph',
-            xaxis=dict(title='Time'),
-            yaxis=dict(title='Value'),
+            title='Fluke3000 FC Readings',
+            xaxis=dict(title='Time (s)'),
+            yaxis=dict(title='Fluke Reading'),
+            xaxis_rangeslider=dict(visible=False),
+            xaxis_range=[xval[0] if xval else datetime.datetime.now(), datetime.datetime.now()]
+        )
+    }
+
+    figure2 = {
+        'data': [go.Scatter(x=xval, y=yval_rolling, mode='lines+markers')],
+        'layout': go.Layout(
+            title='Rolling Average of Last ' + str(ROLLING_AVG_MEASURE) + " Seconds",
+            xaxis=dict(title='Time (s)'),
+            yaxis=dict(title='Fluke Reading'),
             xaxis_rangeslider=dict(visible=False),
             xaxis_range=[xval[0] if xval else datetime.datetime.now(), datetime.datetime.now()]
         )
     }
     
-    return figure
+    return figure1, figure2
 
 # Run the Dash app
 if __name__ == '__main__':
@@ -126,4 +133,3 @@ if __name__ == '__main__':
 # Flush out the cache system
 mult.reset()
 mult.flush()
-print('done')
